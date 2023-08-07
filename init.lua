332c9827e1017634ee8339ea4c3a68d0aef8fa97
olcortesb <olcortesb@gmail.com>
2023-08-07 00:38:37 -0300

    Initial commit

---
+++ init.lua
@@ -0,0 +1,26 @@
+require('craftzdog.base')
+require('craftzdog.highlights')
+require('craftzdog.maps')
+require('craftzdog.plugins')
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+local is_wsl = has "wsl"
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+if is_mac then
+  require('craftzdog.macos')
+end
+if is_win then
+  require('craftzdog.windows')
+end
+if is_wsl then
+  require('craftzdog.wsl')
+end
